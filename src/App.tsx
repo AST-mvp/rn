acbd067c2bff5422f65b1527ace6db6fcf6fad62
CraftyDragon678 <craftydragon678@gmail.com>
2021-02-16 19:03:59 +0900

    feat: wrap with provide auth

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import 'react-native-gesture-handler';
 import { GoogleSignin } from '@react-native-community/google-signin';
 
 import Router from './router';
+import { ProvideAuth } from './hooks/user';
 
 GoogleSignin.configure({
   webClientId: '51227550156-n6v9k0mei8ko0l9jm6crnt5go2r6m9pr.apps.googleusercontent.com',
 });
 
 export default () => (
-  <Router />
+  <ProvideAuth>
+    <Router />
+  </ProvideAuth>
 );
 
