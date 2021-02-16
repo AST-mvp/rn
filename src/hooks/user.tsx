3b6b5a51584b610d36ab45f4f6d151e23102a386
CraftyDragon678 <craftydragon678@gmail.com>
2021-02-16 19:03:47 +0900

    feat: add user hook

---
+++ src/hooks/user.tsx
@@ -0,0 +1,64 @@
+import api from '@src/api';
+import { setToken } from '@src/utils/auth';
+import React, { createContext, useContext, useState, useReducer, useEffect } from 'react';
+
+interface Token {
+  token: string;
+}
+
+type TokenDispatchAction =
+  | { type: 'update'; token: string }
+  | { type: 'refresh' };
+
+interface User {
+  userID: string;
+  username: string;
+}
+
+const useProvideAuth = () => {
+  const [user, setUser] = useState<User | null>();
+  const [token, dispatchToken] = useReducer(
+    (state: Token, action: TokenDispatchAction): Token => {
+      if (action.type === 'refresh') {
+        return { ...state };
+      }
+      const newToken = {
+        token: action.token,
+      };
+      setToken(newToken);
+      return newToken;
+    },
+    { token: '' }
+  );
+
+  useEffect(() => {
+    api.get('/users/me').then((res) => {
+      if (!res.data.login) {
+        return;
+      }
+      setUser(res.data);
+    });
+  }, [token]);
+
+  return {
+    dispatchToken,
+    user,
+  };
+};
+
+const authContext = createContext<ReturnType<typeof useProvideAuth> | undefined>(undefined);
+
+export const ProvideAuth: React.FC = (props) => {
+  const auth = useProvideAuth();
+  return <authContext.Provider value={auth} {...props} />;
+};
+
+const useAuth = () => {
+  const auth = useContext(authContext);
+  if (!auth) {
+    throw new Error('ProvideAuth not found');
+  }
+  return auth;
+};
+
+export default useAuth;
