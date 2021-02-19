bfb624e5fb553c7c3253b6606a5c97bd4afaf6ed
CraftyDragon678 <craftydragon678@gmail.com>
2021-02-19 23:38:37 +0900

    feat: add logout button

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
-import { navigate } from '@src/router/navigator';
+import { navigate, reset } from '@src/router/navigator';
 import { Text } from 'react-native';
 import { readNdef } from '@src/utils/nfc';
 import { useFocusEffect } from '@react-navigation/native';
+import { setToken } from '@src/utils/auth';
+import styled from '@emotion/native';
+
+const Button = styled.TouchableOpacity`
+  padding: 16px;
+  width: 200px;
+  margin: 8px;
+  text-align: center;
+  border: 1px solid black;
+`;
 
 export default () => {
   useFocusEffect(() => {
@@ -17,9 +27,17 @@ export default () => {
     readTag();
   });
 
+  const handleLogout = async () => {
+    await setToken(null);
+    reset('Root');
+  };
+
   return (
     <>
       <Text>태그를 가져다대세요</Text>
+      <Button onPress={handleLogout}>
+        <Text>Logout</Text>
+      </Button>
     </>
   );
 };
