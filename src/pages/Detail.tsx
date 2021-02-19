b3d4828cce82bf1a763144f9dcb88f92462eeccb
CraftyDragon678 <craftydragon678@gmail.com>
2021-02-19 23:50:59 +0900

    feat(detail): fetch and show data

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,11 +1,17 @@
+import React, { useEffect, useState } from 'react';
 import { RouteProp } from '@react-navigation/native';
+import api from '@src/api';
 import { RootStackParamList } from '@src/router/navigator';
+import { Text } from 'react-native';
 
 type Props = {
   route: RouteProp<RootStackParamList, 'Detail'>;
 }
 
 export default ({ route: { params: { nfcId }}}: Props) => {
-  console.log(nfcId);
-  return null;
+  const [st, setSt] = useState('');
+  useEffect(() => {
+    api.get(`/products/${nfcId}`).then(({ data }) => setSt(data));
+  }, [nfcId]);
+  return <Text>{JSON.stringify(st)}</Text>;
 };
