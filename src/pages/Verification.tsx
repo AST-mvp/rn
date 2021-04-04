9a55cd3050825cf7d2f2960c622e587e76d26a39
CraftyDragon678 <craftydragon678@gmail.com>
2021-04-04 14:14:42 +0900

    feat: test verification id

--- src/pages/Verification.tsx
+++ src/pages/Verification.tsx
@@ -21,7 +21,7 @@ export default () => {
     readTag();
   });
 
-  const handleTestButton = () => navigate('Detail', { nfcId: '1000' });
+  const handleTestButton = () => navigate('Detail', { nfcId: '1008' });
 
   return (
     <>
