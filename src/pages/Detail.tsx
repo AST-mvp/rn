8d18dbe162b065472af7d424ee3542981c2b6b0e
CraftyDragon678 <craftydragon678@gmail.com>
2021-04-06 20:13:28 +0900

    feat(detail): use Product type

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,17 +1,26 @@
-import React, { useEffect, useState } from 'react';
-import { RouteProp } from '@react-navigation/native';
+import React, {useEffect, useState} from 'react';
+import {RouteProp} from '@react-navigation/native';
 import api from '@src/api';
-import { RootStackParamList } from '@src/router/navigator';
-import { Text } from 'react-native';
+import {RootStackParamList} from '@src/router/navigator';
+import {Text} from 'react-native';
+import {Product} from '@src/constants/types';
 
 type Props = {
   route: RouteProp<RootStackParamList, 'Detail'>;
-}
+};
 
-export default ({ route: { params: { nfcId }}}: Props) => {
-  const [st, setSt] = useState('');
+export default ({
+  route: {
+    params: {nfcId},
+  },
+}: Props) => {
+  const [product, setProduct] = useState<Product>();
   useEffect(() => {
-    api.get(`/products/${nfcId}`).then(({ data }) => setSt(data.result));
+    api.get(`/products/${nfcId}`).then(({data}) => setProduct(data.result));
   }, [nfcId]);
-  return <Text>{JSON.stringify(st)}</Text>;
+  return (
+    <>
+      <Text>{JSON.stringify(product)}</Text>
+    </>
+  );
 };
