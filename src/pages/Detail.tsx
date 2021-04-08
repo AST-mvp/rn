9254d7af48d4a992647fd12a1384cd910702bbd2
CraftyDragon678 <craftydragon678@gmail.com>
2021-04-08 13:15:04 +0900

    test: ...

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -4,16 +4,23 @@ import api from '@src/api';
 import { navigate, RootStackParamList } from '@src/router/navigator';
 import { Button, Text } from 'react-native';
 import { Product } from '@src/constants/types';
+import styled from '@emotion/native';
+import useAuth from '@src/hooks/user';
 
 type Props = {
   route: RouteProp<RootStackParamList, 'Detail'>;
 };
+
+const Row = styled.View`
+  flex-direction: row;
+`;
 
 export default ({
   route: {
     params: { nfcId },
   },
 }: Props) => {
+  const { user } = useAuth();
   const [product, setProduct] = useState<Product | null>();
   useEffect(() => {
     api
@@ -31,7 +38,17 @@ export default ({
       <Text>brand: {product.brandID}</Text>
       <Text>product: {product.productID}</Text>
       <Text>{product.manufactureDate}</Text>
-      <Button title="trade" onPress={handleTrade} />
+      {user?.username === 'test3' ? (
+        <>
+          <Text>제품 거래</Text>
+          <Row>
+            <Button onPress={() => {}} title="확인" />
+            <Button onPress={() => {}} title="취소" />
+          </Row>
+        </>
+      ) : (
+        <Button title="trade" onPress={handleTrade} />
+      )}
     </>
   );
 };
