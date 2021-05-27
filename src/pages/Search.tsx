6578455bb30c16fe494ce6a1a61a4880905238f6
ph8nt0m <phant0m@kakao.com>
2021-05-27 20:19:54 +0900

    Update Search.tsx

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -44,14 +44,13 @@ const Search = () => {
     }, [changeTheme, resetTheme]),
   );
 
-
   return (
     <>
       <HeaderContainer>
         <SearchContainer>
           <TextInput 
             value={searchData}
-            onChangeText={(searchData) => setSearchData(searchData)}
+            onChangeText={setSearchData}
             placeholder={'검색어를 입력해주세요.'}
           />
           <SearchIcon source={searchIcon} />
