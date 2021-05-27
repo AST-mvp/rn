9e7d60e584f18c453d3ffb313b1745a9db35fdbe
ph8nt0m <phant0m@kakao.com>
2021-05-27 20:26:38 +0900

    Change Button Style

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,8 +1,22 @@
 import Button from '@src/components/Button';
+import styled from '@emotion/native';
 import useAuth from '@src/hooks/user';
 import { navigate, reset } from '@src/router/navigator';
 import { setToken } from '@src/utils/auth';
 import React from 'react';
+
+const Container = styled.View`
+  flex: 1;
+  padding: 20px;
+`;
+
+const LogOut = styled.View`
+  marginTop: 160%;
+`;
+
+const AdminDrop = styled.View`
+  marginTop: 1.5%;
+`;
 
 const Profile = () => {
   const { user } = useAuth();
@@ -17,12 +31,16 @@ const Profile = () => {
   };
 
   return (
-    <>
-      <Button onPress={handleLogout} text="logout" />
-      {user?.username === 'admin' && (
+    <Container>
+      <LogOut>
+        <Button onPress={handleLogout} text="logout" />
+      </LogOut>
+      <AdminDrop>
+        {user?.username === 'admin' && (
         <Button onPress={handleAdminDrop} text="admin drop" />
-      )}
-    </>
+        )}
+      </AdminDrop>
+    </Container>
   );
 };
 
